fdf8d4d791b2ba835febff4ffd293d32c853dacc
marnix <mvsoom@gmail.com>
2024-04-01 01:16:49 +0200

    Cleanup

--- src/slow/reddit/scrape_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NUM_THREADS=4
-STRIDE=259200 # 3 days
-MAXFSIZE=2 # GB
-
-cat subreddit/subreddits.list | xargs -I {} -P $NUM_THREADS -n 1 \
-    python scrape.py {} subreddit/{}.csv --stride $STRIDE --maxfsize $MAXFSIZE --verbose --update
