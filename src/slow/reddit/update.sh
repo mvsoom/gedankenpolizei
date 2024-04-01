acf54bd30c19c58bd5bd29b3f1038f9d48d4fc0f
marnix <mvsoom@gmail.com>
2024-04-01 13:31:11 +0200

    Allow commenting out subreddits

--- src/slow/reddit/update.sh
+++ src/slow/reddit/update.sh
@@ -7,14 +7,14 @@ POSTS_FILE="posts/posts.h5"
 POSTS_BATCH=1000
 
 SCRAPE_NUM_THREADS=4
-SCRAPE_STRIDE=259200 # 3 days
+SCRAPE_STRIDE=31449600 # 1 year
 SCRAPE_MAXFSIZE=5 # GB
 
 echo "Relabeling existing posts..."
 python relabel.py "$POSTS_FILE" --verbose
 
 echo "Updating new scrapes..."
-cat "$SUBREDDIT_DIR"/subreddits.list | xargs -I {} -P $SCRAPE_NUM_THREADS -n 1 \
+cat "$SUBREDDIT_DIR"/subreddits.list | grep -v '^#' | xargs -I {} -P $SCRAPE_NUM_THREADS -n 1 \
     python scrape.py {} "$SUBREDDIT_DIR"/{}.csv --stride $SCRAPE_STRIDE --maxfsize $SCRAPE_MAXFSIZE --verbose --update
 
 echo "Normalizing new scrapes..."
