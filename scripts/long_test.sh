17b949d3a480f0aa43f942a5ddc13f112062cf7c
marnix <mvsoom@gmail.com>
2024-10-03 18:31:17 +0200

    Use stdout not stderr to avoid displaying errors

--- scripts/long_test.sh
+++ scripts/long_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Settings used for the demo; open client/client.html in a browser for fancy output
+echo $(date)
 grab/websocket | \
 python -m src.fast.narrate \
     --config fast.novelty_threshold:10 \
@@ -10,6 +10,8 @@ python -m src.raw.stream \
     --config raw.memory_size:128 \
     --config slow.pace:0.5 \
     --config raw.pace:16 \
-    --config log.level:DEBUG \
-    --roll-tape \
-    2> >(tee long_test_output.txt | emit/websocket)
+    --config log.level:DEBUG | \
+monitor/text | \
+tee long_test_output.txt | \
+monitor/record long_test_output.jsonl --echo | \
+emit/websocket
