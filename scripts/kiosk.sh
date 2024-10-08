8c1b47f46f10c83717f522515cd876c7a2d2956b
marnix <mvsoom@gmail.com>
2024-10-08 19:19:20 +0200

    Implement prototypes

---
+++ scripts/kiosk.sh
@@ -0,0 +1,15 @@
+chromium-browser "$1" \
+  --start-fullscreen \
+  --kiosk \
+  --incognito \
+  --noerrdialogs \
+  --disable-translate \
+  --no-first-run \
+  --fast \
+  --fast-start \
+  --disable-infobars \
+  --disable-features=TranslateUI \
+  --disk-cache-dir=/dev/null \
+  --overscroll-history-navigation=0 \
+  --disable-pinch \
+  --autoplay-policy=no-user-gesture-required
