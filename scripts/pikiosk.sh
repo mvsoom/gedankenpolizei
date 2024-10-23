436d91fe766c3aae931a6a758af4b6d3fe356b70
marnix <mvsoom@gmail.com>
2024-10-23 23:50:30 +0200

    Working Pi launch setup

--- scripts/pikiosk.sh
+++ scripts/pikiosk.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Set up a trap to kill all child processes when the script exits or when CTRL-C is pressed
+trap "kill 0" EXIT SIGINT
+
+# Start the pipeline in the background
+grab/webcam | \
+python -m src.fast.narrate \
+    --config fast.novelty_threshold:10 \
+    --jsonl \
+    --output-frames | \
+python -m src.raw.stream \
+    --config raw.memory_size:128 \
+    --config slow.pace:0.5 \
+    --config raw.pace:16 | \
+emit/websocket &
+
+# Start chromium-browser in the foreground
 chromium-browser client/pi.html \
   --start-fullscreen \
   --kiosk \
@@ -15,3 +32,5 @@ chromium-browser client/pi.html \
   --disable-pinch \
   --autoplay-policy=no-user-gesture-required \
   --remote-debugging-port=9222
+
+# When chromium-browser exits, the script exits, triggering the trap which kills all child processes
